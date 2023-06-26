7f1e519d85fbe4dac1a28d9a41ca7a64fb5a6deb
Андреева Мария <andreeva.m@informatics.ru>
2023-06-26 19:28:51 +0300

    Classwork 1_2

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -6,6 +6,6 @@ Console.WriteLine("Write a number: ");
 // string num_str = Console.ReadLine()!;
 
 // int num = int.Parse(num_str);
-int num = int.Parse(Console.ReadLine());
+int num = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine(num * num);
